0deccdefb5bac9eb2eef947214cdc2019a2a6445
MathieuCeren <cerenmathieu@gmail.com>
2020-10-30 10:07:52 +0100

    feat(core): add routing

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
   return (
     <React.Fragment>
-      <div className="header">
+      <div className="container header">
         <h1>Ego Quizz</h1>
+        <span>
+          <Link to="/">Accueil</Link>
+          <Link to="/score">Leaderboards</Link>
+          <Link to="/teams">Notre équipe</Link>
+        </span>
       </div>
     </React.Fragment>
   );
---
+++ src/Components/Leaderboards/Leaderboards.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Leaderboards() {
+  return (
+    <div className="Leaderboards">Leaderboards</div>
+  )
+}
+
+export default Leaderboards;
