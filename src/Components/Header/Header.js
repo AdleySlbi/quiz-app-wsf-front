a5903b2774f7ddfd021b85b30cd1a56ef2a34a12
MathieuCeren <cerenmathieu@gmail.com>
2020-10-30 10:27:18 +0100

    feat(header): style logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,9 @@ function Header() {
   return (
     <React.Fragment>
       <div className="container header">
-        <h1>Ego Quizz</h1>
+        <span className="logo">
+          Notre <b>EGO-QUIZ</b>
+        </span>
         <span>
           <Link to="/">Accueil</Link>
           <Link to="/score">Leaderboards</Link>
