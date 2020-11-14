97bc99ba8d5ba70805bc23a398aea326689349d0
MathieuCeren <cerenmathieu@gmail.com>
2020-11-14 20:22:28 +0100

    fix(leaderboards): add css on scores

--- src/Components/Leaderboards/Leaderboards.js
+++ src/Components/Leaderboards/Leaderboards.js
@@ -8,21 +8,37 @@ function Leaderboards() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
 
+  const colors = [
+    "#a753f6",
+    "#d7757e",
+    "#ffbc48",
+    "#1abffa",
+    "#e49c6d",
+    "#a1958c",
+    "#a8ea28",
+    "#f732e7",
+  ];
+
+  let currentColor = null;
+  const randomColor = () => {
+    currentColor = colors[(Math.random() * colors.length) >> 0];
+    return currentColor;
+  };
+
   useEffect(() => {
     // const url = 'https://adley-quizz.herokuapp.com/api/scores';
     // const url = 'https://polar-ocean-73785.herokuapp.com/api/scores/60';
 
     Promise.all(
       [
-        fetch('https://wsf-popcorn-backend.herokuapp.com/api/scores'),
-        fetch('https://polar-ocean-73785.herokuapp.com/api/scores/60')
+        fetch('https://wsf-popcorn-backend.herokuapp.com/api/scores/3'),
+        fetch('https://polar-ocean-73785.herokuapp.com/api/scores/3')
       ]
     )
     .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
     .then(([data1, data2]) => {
             setArrayScores1(data1.scores)
-            setArrayScores2(data2.data)
-            console.log(data2.data)
+            setArrayScores2(data2.questions)
             setIsLoaded(true)
         }, 
         (error) => {
@@ -61,14 +77,16 @@ function Leaderboards() {
         </div>
         <div className="col-12 col-sm-6">
           {array_scores1  ? (array_scores1.map((field, i) => (
-              <span className="row" key={i}>
-                #{field.username} {field.score}
+              <span className="score-row row mt-2 p-1" style={{ background: randomColor() }} key={i}>
+                <p className="m-0 p-0 pl-5" style={{ textTransform: 'capitalize'}}>{field.username} </p>
+                <p className="m-0 p-0 pr-5">score : {field.score}</p>
               </span>)) ) 
             : (<></>)
           }
           {array_scores2  ? (array_scores2.map((field, i) => (
-              <span className="row" key={i}>
-                #{field.username} {field.score}
+              <span className="score-row row mt-2 p-1" style={{ background: randomColor() }} key={i}>
+                <p className="m-0 p-0 pl-5" style={{ textTransform: 'capitalize'}}>{field.username} </p>
+                <p className="m-0 p-0 pr-5">score :{field.score}</p>
               </span>)) ) 
             : (<></>)
           }
