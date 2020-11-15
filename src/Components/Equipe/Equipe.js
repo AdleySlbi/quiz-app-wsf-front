8ab77ee4b5642a476ad42292358a2798e317ffa5
MathieuCeren <cerenmathieu@gmail.com>
2020-11-15 16:07:53 +0100

    feat(equipe): add member teams page

--- src/Components/Equipe/Equipe.js
+++ src/Components/Equipe/Equipe.js
@@ -1,9 +1,24 @@
 import React from "react";
+import "./Equipe.css";
 
 function Equipe () {
   return (
     <div className="equipe">
-      Equipe
+      <h1>Louis Laugier </h1>
+      <p>Responsable du back-end en Go, </p>
+      <h1>Adley Salabi</h1>
+      <p>Responsable du front-end et de la création des questions du quizz</p>
+      <h1>Mathieu Ceren </h1>
+      <p>Responsable du front-end et de la création des maquettes </p>
+      <h1>Alexis Van Hoorde</h1>
+      <p>Chef de projet et responsable de la création des livrables </p>
+
+
+
+
+
+
+
     </div>
   )
 }
